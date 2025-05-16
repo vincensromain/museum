ec350cd36fe2974b4d7eda3259269777682bedd4
vincensromain <vincensromainpro@gmail.com>
2025-05-16 09:00:36 +0200

    add loader

--- src/app/components/Modal/Modal.jsx
+++ src/app/components/Modal/Modal.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import IconMuseum from "../IconsMuseum/IconsMuseum";
 import Link from "next/link";
+import "./Modal.scss";
 
 const Modal = ({ showReturn, content, lastViewedOrbRef, modalAppear }) => {
   return (
@@ -9,35 +10,33 @@ const Modal = ({ showReturn, content, lastViewedOrbRef, modalAppear }) => {
         {content.artworks[lastViewedOrbRef.current] && (
           <>
             <div className="icon">
-              <IconMuseum
-                className="svg_scan"
-                icon="svgScan"
-                width={56}
-                height={56}
-              />
+              <IconMuseum icon="svgScan" width={56} height={56} />
               <IconMuseum
                 icon={content.artworks[lastViewedOrbRef.current].icon}
                 width={30}
                 height={30}
               />
             </div>
-            <h2 className="artwork_name">
-              {content.artworks[lastViewedOrbRef.current].title}
-            </h2>
-            <p className="artwork_description">
-              {content.artworks[lastViewedOrbRef.current].description}
-            </p>
-            <div className="hypertext">
-              <Link
-                href={content.artworks[lastViewedOrbRef.current].link}
-                className="hypertext_link"
-              >
-                Je n'ai pas accès à la puce NFC
-              </Link>
+
+            <div className="artwork_content">
+              <h2 className="artwork_name">
+                {content.artworks[lastViewedOrbRef.current].title}
+              </h2>
+              <p className="artwork_description">
+                {content.artworks[lastViewedOrbRef.current].description}
+              </p>
+              <div className="hypertext">
+                <Link
+                  href={content.artworks[lastViewedOrbRef.current].link}
+                  className="hypertext_link"
+                >
+                  Je n'ai pas accès à la puce NFC
+                </Link>
+              </div>
             </div>
           </>
         )}
-        <button onClick={modalAppear}>Fermer</button>
+        {/* <button onClick={modalAppear}>Fermer</button> */}
       </div>
     </div>
   );
