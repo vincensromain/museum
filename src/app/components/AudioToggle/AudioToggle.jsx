888ebed271a8bd60182124a4c9899e7cd2155bca
vincensromain <vincensromainpro@gmail.com>
2025-06-13 12:25:08 +0200

    audio is now not stop when toggled

--- src/app/components/AudioToggle/AudioToggle.jsx
+++ src/app/components/AudioToggle/AudioToggle.jsx
@@ -18,49 +18,29 @@ export default function AudioToggleButton() {
       const audios = document.querySelectorAll("audio");
       audios.forEach((audio) => {
         if (event.detail) {
-          if (audio.src.includes("audio.wav")) {
-            // Si c'est le fichier audio.wav, on met le volume à 0.1
-            gsap.to(audio, {
-              volume: 0.1,
-              duration: 0.5,
-              ease: "power1.out",
-              onStart: () => {
-                audio
-                  .play()
-                  .catch((err) => console.warn("Error playing audio:", err));
-              },
-            });
-          } else {
-            // Pour les autres fichiers audio, on met le volume à 1
-            gsap.to(audio, {
-              volume: 1,
-              duration: 0.5,
-              ease: "power1.out",
-              onStart: () => {
-                audio
-                  .play()
-                  .catch((err) => console.warn("Error playing audio:", err));
-              },
-            });
-          }
+          // remise à l’écoute
+          const isWav = audio.src.includes("audio.wav");
+          gsap.to(audio, {
+            volume: isWav ? 0.1 : 1,
+            duration: 0.5,
+            ease: "power1.out",
+            onStart: () =>
+              audio
+                .play()
+                .catch((err) => console.warn("Error playing audio:", err)),
+          });
         } else {
           gsap.to(audio, {
             volume: 0,
             duration: 0.5,
             ease: "power1.in",
-            onComplete: () => {
-              audio.pause();
-            },
           });
         }
       });
     };
 
     window.addEventListener("toggleAudio", handleToggleAudio);
-
-    return () => {
-      window.removeEventListener("toggleAudio", handleToggleAudio);
-    };
+    return () => window.removeEventListener("toggleAudio", handleToggleAudio);
   }, []);
 
   return (
